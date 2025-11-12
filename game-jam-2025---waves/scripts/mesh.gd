8e3a943e55114d2fe1b6876c5fadfc426076e52d
William <williamcraske@gmail.com>
2025-11-12 13:42:14 -0800

    boat recenters after changing axis

--- game-jam-2025---waves/scripts/mesh.gd
+++ game-jam-2025---waves/scripts/mesh.gd
@@ -1,4 +1,6 @@
 # adapted from How to make things float in Godot 4: https://www.youtube.com/watch?v=_R2KDcAp1YQ&t=200s
+# MAKE BOAT ONLY BE ABLE TO MOVE WHEN TOUCHING WATER
+
 extends RigidBody3D
 
 @export var float_force := 1.5
@@ -7,10 +9,15 @@ extends RigidBody3D
 @export var use_probes := false
 
 #movement settings
-@export var moveSpeed := 20.0
+@export var moveSpeed := 100.0
 @export var boostMod := 3.0
 @export var turnSpeed := 0.03
+@export var recoverSpeed := 2.0  
+
 #trick settings
+var totalScore = 0.0
+var touchingWater = true
+var trickAngles = [180, 360, 720, 1080]
 
 
 
@@ -59,13 +66,18 @@ func _physics_process(delta):
 		
 	#tricks
 	if Input.is_action_pressed("uarrow"):
-		add_constant_torque(transform.basis.x * turnSpeed * boostMod)
+		apply_torque_impulse(transform.basis.x * turnSpeed)
 	if Input.is_action_pressed("darrow"):
-		add_constant_torque(-transform.basis.x * turnSpeed * boostMod)
+		apply_torque_impulse(-transform.basis.x * turnSpeed)
 	if Input.is_action_pressed("rarrow"):
-		add_constant_torque(transform.basis.z * turnSpeed * boostMod)
+		apply_torque_impulse(transform.basis.z * -turnSpeed)
 	if Input.is_action_pressed("larrow"):
-		add_constant_torque(-transform.basis.z * turnSpeed * boostMod)
+		apply_torque_impulse(transform.basis.z * turnSpeed)
+		
+	#when not doing tricks, 
+	if not (Input.is_action_pressed("uarrow") or Input.is_action_pressed("darrow") or Input.is_action_pressed("larrow") or Input.is_action_pressed("rarrow")):
+		recoverBoat(delta)
+
 
 		
 	submerged = false
@@ -94,8 +106,27 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 		state.linear_velocity *= 1.0 - water_drag
 		state.angular_velocity *= 1.0 - water_angular_drag
 
+func recoverBoat(delta):
+	var boat = get_node("../Boat")
+	#get boats cureent basis
+	var curBasis = boat.transform.basis
+	#get current y rotation of boat
+	var curRotationY = curBasis.get_euler().y
+	#create target basis where boat upright, but still facing correct direction
+	var targetBasis = Basis(Vector3.UP, curRotationY)
+	#interpolate from current to upright
+	boat.transform.basis = curBasis.slerp(targetBasis, recoverSpeed * delta)
+
+
 
 func crazyAssTricks():
 	var boat = get_node_or_null("../Boat")
+	if touchingWater == false:
+		var yes = 0
+	#fill out later with when not touching water, track the angles rotated
+	#maybe use an array of angles, [180, 360, 720, 1080], and more 
+	#set flags when angle passses x amount
+	#add scores based on tricks, with multiplier based on tricks within a timer that starts after landing first trick
+	#then add to total score
 	
 	
