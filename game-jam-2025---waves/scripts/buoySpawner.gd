fd43453414e2486c6a856702d31878b7744da64e
ok-sho <93733491+ok-sho@users.noreply.github.com>
2025-11-13 14:01:13 -0800

    switch to longer path

--- game-jam-2025---waves/scripts/buoySpawner.gd
+++ game-jam-2025---waves/scripts/buoySpawner.gd
@@ -1,10 +1,11 @@
 extends Path3D
 
 @export var buoy_scene: PackedScene = preload("res://scenes/buoy.tscn")
-@export var buoy_spacing: float = 10.0
+@export var buoy_spacing: float = 25.0
 @export var track_width: float = 60.0
 @export var wall_height: float = 50.0
 @export var water_path: NodePath = "../WaterMesh"
+@export var num_points_in_wall = 200 # more points = smoother curves
 
 var water: MeshInstance3D
 
@@ -13,6 +14,9 @@ func _ready():
 	
 	if not water:
 		push_error("Water node not found at path: " + str(water_path))
+		return
+	if not curve.sample_baked(0.0):
+		push_error("Uh uh, no points in Curve3D")
 		return
 	
 	spawn_buoys()
@@ -66,12 +70,10 @@ func create_wall_side_mesh(perpendicular_offset: float, wall_name: String):
 	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	
 	var curve_length = curve.get_baked_length()
-	# more pts = smoother
-	var num_points = 100
 	
 	# build wall mesh along the path
-	for i in range(num_points):
-		var offset = (i / float(num_points - 1)) * curve_length
+	for i in range(num_points_in_wall):
+		var offset = (i / float(num_points_in_wall - 1)) * curve_length
 		var pos = curve.sample_baked(offset)
 		
 		# get perpendicular direction 
@@ -83,8 +85,8 @@ func create_wall_side_mesh(perpendicular_offset: float, wall_name: String):
 		pos += right * perpendicular_offset
 		
 		# create vertical wall quad
-		if i < num_points - 2: # - 2 avoids loop issue where it blocks off route, fix later
-			var next_offset = ((i + 1) / float(num_points - 1)) * curve_length
+		if i < num_points_in_wall - 2: # - 2 avoids loop issue where it blocks off route, fix later
+			var next_offset = ((i + 1) / float(num_points_in_wall - 1)) * curve_length
 			var next_pos = curve.sample_baked(next_offset)
 			
 			var next_forward = curve.sample_baked(min(next_offset + 1, curve_length)) - next_pos
