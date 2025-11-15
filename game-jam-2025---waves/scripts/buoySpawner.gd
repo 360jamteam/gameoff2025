c6196a9bc3a111ed926247d9db6c03b25e218dd1
ok-sho <93733491+ok-sho@users.noreply.github.com>
2025-11-14 21:40:23 -0800

    increase recoverBoat() torque to keep boat from getting stuck upside down in the water; move applications of force/torque on boat rigid body to _integrate_physics() per docs; add general linear and angular velocity damping; change boat mesh scale from 0.5 to 0.75; remove scaling on boat CollisionShape3D and resize it with radius and height instead; tune driving speeds and track width, height, wall friction

--- game-jam-2025---waves/scripts/buoySpawner.gd
+++ game-jam-2025---waves/scripts/buoySpawner.gd
@@ -2,8 +2,8 @@ extends Path3D
 
 @export var buoy_scene: PackedScene = preload("res://scenes/buoy.tscn")
 @export var buoy_spacing: float = 25.0
-@export var track_width: float = 60.0
-@export var wall_height: float = 50.0
+@export var track_width: float = 120.0
+@export var wall_height: float = 100.0
 @export var water_path: NodePath = "../WaterMesh"
 @export var num_points_in_wall = 200 # more points = smoother curves
 
@@ -60,7 +60,7 @@ func create_wall_side_mesh(perpendicular_offset: float, wall_name: String):
 	#make the wall less friction-y so the boat doesn't stick to it
 	var physics_object = PhysicsMaterial.new()
 	wall.physics_material_override = physics_object
-	physics_object.friction = 0.1
+	physics_object.friction = 0.01
 	
 	# mesh for wall
 	var mesh_instance = MeshInstance3D.new()
@@ -133,7 +133,7 @@ func create_wall_side_mesh(perpendicular_offset: float, wall_name: String):
 	
 	# make it kinda see through for debug
 	var material = StandardMaterial3D.new()
-	material.albedo_color = Color(1, 0, 0, 0.3) 
+	material.albedo_color = Color(1, 0, 0, 0.15) 
 	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
 	material.cull_mode = BaseMaterial3D.CULL_DISABLED
 	mesh_instance.material_override = material
